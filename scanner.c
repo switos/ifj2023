87b709a353a0f698bdf950c8e0b1eb24d0b1f115
switos <atomol112@gmail.com>
2023-09-23 10:43:01 +0200

    IdentifierState scanner feature

---
+++ scanner.c
@@ -0,0 +1,35 @@
+#include "string.h"
+
+
+int identifierState(char symbol) {
+    if ((symbol>64&&symbol<91) || (symbol>96 && symbol<123) || (symbol>47 && symbol<58) || (symbol == 95) ) {
+        symbol = getc();
+        identifierState(symbol);
+    } else {
+        return 0; //succes, return the identifier, clean buffer 
+    }
+}
+
+int underscroreState(char symbol) {
+    if ((symbol>64&&symbol<91) || (symbol>96 && symbol<123) || (symbol>47 && symbol<58)) {
+            symbol = getc();
+            identifierState(symbol);
+    } else {
+        return 1; //error 
+    }
+}
+
+int start_state( char symbol) {
+    if(symbol == "_") {
+            symbol = getc();
+            underscoreState();
+    } else if ((symbol>64&&symbol<91) || (symbol>96 && symbol<123)) {
+            symbol = getc();
+            identifierState(symbol);
+    }
+    
+}
+
+int get_token() {
+    char symbol = getc();
+}
