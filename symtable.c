bbe39ce3dc2814e86f1628094dceeedae803e4e3
HelHeim111 <dwp2004@icloud.com>
2023-10-09 18:41:13 +0200

    symtable v1

--- symtable.c
+++ symtable.c
@@ -19,7 +19,7 @@ symtable_t* symtable_init() {
     table->sizeAllocated = INIT_SIZE;
     table->sizeUsed = 0;
 
-    table->bucket = (htab_item_t**)malloc(sizeof(htab_item_t*) * INIT_SIZE);
+    table->bucket = (htab_item_t**)calloc(INIT_SIZE, sizeof(htab_item_t*));
     if(table->bucket == NULL) {
         fprintf(stderr, "Failed to allocate memory");
     }
@@ -31,5 +31,106 @@ symtable_t* symtable_init() {
 }
 
 void symtable_resize(symtable_t* table, int newTableSize) {
-   
+   htab_item_t** newBucket = (htab_item_t**)calloc(newTableSize, sizeof(htab_item_t*));
+   if(newBucket == NULL) {
+        fprintf(stderr, "Failed to allocate memory");
+    }
+
+    for(int i = 0; i < table->sizeAllocated; i++) {
+        htab_item_t* tmp = table->bucket[i];
+        while(tmp != NULL) {
+            htab_item_t* tmpNext = tmp->next;
+            unsigned int index = get_hash(tmp->key, newTableSize);
+            tmp->next = newBucket[index];
+            newBucket[index] = tmp;
+            tmp = tmpNext;
+        }
+    }
 }
+
+htab_data_t* symtable_insert_variable(symtable_t* table, char* key, char* type, char* name, char* value, bool defined, bool constant){
+    if(table->sizeUsed / table->sizeAllocated > THRESHOLD) {
+        resize(table, table->sizeAllocated * 2);
+    }
+
+    unsigned int index = get_hash(key, table->sizeAllocated);
+
+    htab_item_t* newBucket = (htab_item_t*)malloc(sizeof(htab_item_t));
+    if(newBucket == NULL) {
+        fprintf(stderr, "Failed to allocate memory");
+    }
+
+    strcpy(newBucket->data->var->name, name);
+    strcpy(newBucket->data->var->type, type);
+    strcpy(newBucket->data->var->value, value);
+    newBucket->data->var->defined = defined;
+    newBucket->data->var->constant = constant;
+
+    table->bucket[index] = newBucket;
+    table->sizeUsed++;
+
+}
+
+htab_data_t* symtable_insert_func(symtable_t* table, char* key, char* returnType, char* name, bool defined, int argumentAmount){
+    if(table->sizeUsed / table->sizeAllocated > THRESHOLD) {
+        resize(table, table->sizeAllocated * 2);
+    }
+
+    unsigned int index = get_hash(key, table->sizeAllocated);
+
+    htab_item_t* newBucket = (htab_item_t*)malloc(sizeof(htab_item_t));
+    if(newBucket == NULL) {
+        fprintf(stderr, "Failed to allocate memory");
+    }
+
+    strcpy(newBucket->data->func->name, name);
+    strcpy(newBucket->data->func->returnType, returnType);
+    newBucket->data->func->defined = defined;
+    newBucket->data->func->argumentAmount = argumentAmount;
+    newBucket->data->func->argumentsInArray = 0;
+    newBucket->data->func->param = (data_param_t*)malloc(sizeof(data_param_t) * 2);
+    if(newBucket->data->func->param == NULL) {
+        fprintf(stderr, "Failed to allocate memory");
+    }
+
+    table->bucket[index] = newBucket;
+    table->sizeUsed++;
+
+}
+
+bool symtable_add_arguments(data_func_t* func, char* name, char* identifier, char* type) {
+    int pos = func->argumentsInArray;
+    strcpy(func->param[pos].identifier, identifier);
+    strcpy(func->param[pos].name, name);
+    strcpy(func->param[pos].type, type);
+}
+
+htab_data_t* symtable_search (symtable_t* table, char* key) {
+    if(table == NULL || key == NULL) {
+        return NULL;
+    }
+
+    unsigned int index = get_hash(key, table->sizeAllocated);
+    htab_item_t* tmp = table->bucket[index];
+
+    while(tmp != NULL) {
+        if(!strcmp(tmp->key, key)) {
+            return &tmp->data;
+        }
+        tmp = tmp->next;
+    }
+}
+
+void symtable_free (symtable_t* table) {
+    for(int i = 0; i < table->sizeAllocated; i++) {
+        htab_item_t* current = table->bucket[i];
+        while(current != NULL) {
+            htab_item_t* tmp = current;
+            current = current->next;
+            free(tmp);
+        }
+    }
+    free(table->bucket);
+    free(table);
+}
+
--- symtable.h
+++ symtable.h
@@ -3,16 +3,41 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "stdbool.h"
 
 #define INIT_SIZE 8     //initial size for symtable
 #define THRESHOLD 0.7   //if symtable is filled above this percentage, it is being resized twice its current size 
 
 typedef struct {
-    int smth;
+    char* type;
+    char* name;
+    char* value;
+    bool defined;
+    bool constant;
+}data_variable_t;
+
+typedef struct {
+    char* name;
+    char* identifier;
+    char* type;
+}data_param_t;
+
+typedef struct {
+    char* returnType;
+    char* name;
+    bool defined;
+    int argumentAmount;
+    int argumentsInArray;
+    data_param_t* param;
+}data_func_t;
+
+typedef struct {
+    data_variable_t* var;
+    data_func_t* func;   
 }htab_data_t;
 
 typedef struct htab_item{
-    htab_data_t data;
+    htab_data_t* data;
     struct htab_item* next;
     char* key;
 }htab_item_t;
@@ -29,6 +54,14 @@ symtable_t* symtable_init();
 
 void symtable_resize(symtable_t* table, int newTableSize);
 
+htab_data_t* symtable_insert_variable(symtable_t* table, char* key, char* type, char* name, char* value, bool defined, bool constant);
 
+htab_data_t* symtable_insert_func(symtable_t* table, char* key, char* returnType, char* name, bool defined, int argumentAmount);
+
+bool symtable_add_arguments(data_func_t* func, char* name, char* identifier, char* type);
+
+void symtable_free (symtable_t* table);
+
+htab_data_t* symtable_search (symtable_t* table, char* key);
 
 #endif SYMTABLE_H
